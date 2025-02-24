9198eb6efa7481f8b7414129a3a0b61c60182e29
Rahim10020 <rahialighi@gmail.com>
2025-02-24 11:35:22 +0000

    Finition du header et debut de search

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
 // Grab elements
-const selectElement = selector => {
-  const element = document.querySelector(selector)
-  if (element) return element
-  throw new Error(`Something went wrong. Make sure the ${selector} exists or is typed correctly`)
-}
+const headerElement = document.querySelector('#header')
+const menuToggleIcon = document.querySelector('#menu-toggle-icon')
+
 
 // Nav styles on scroll
 const scrollHeader = () => {
-  const headerElement = selectElement('#header')
-  if (this.scrollY >= 15){
+  if (this.scrollY >= 15) {
     headerElement.classList.add('activated')
-  }else {
+  } else {
     headerElement.classList.remove('activated')
   }
 }
-
 window.addEventListener('scroll', scrollHeader)
 
+
 // open menu and search popup
-const menuToggleIcon = selectElement('')
+const toggleMenu = () => {
+  const mobileMenu = document.querySelector('#menu')
+  mobileMenu.classList.toggle('activated')
+  menuToggleIcon.classList.toggle('activated')
+}
+menuToggleIcon.addEventListener('click', toggleMenu)
 // open/close search form group
+const formOpenButton = document.querySelector('#search-icon')
+const formCloseButton = document.querySelector('#form-close-btn')
+const searchFormContainer = document.querySelector('#search-form-container')
+
+formOpenButton.addEventListener('click', () => searchFormContainer.classList.add('activated'))
+
+// close search from popup with ESC key press
+// switch theme/ add to local storage
+const bodyElement = document.body
+const themeToggleButton = document.querySelector('#theme-toggle-button')
+const currentTheme = localStorage.getItem('currentTheme')
+
+if (currentTheme === 'themeActive') {
+  bodyElement.classList.add('light-theme')
+}
+
+themeToggleButton.addEventListener('click', () => {
+  bodyElement.classList.toggle('light-theme')
+  if (bodyElement.classList.contains('light-theme')) {
+    localStorage.setItem('currentTheme', 'themeActive')
+  } else {
+    localStorage.removeItem('currentTheme')
+  }
+})
+
+
+// swiper
+
